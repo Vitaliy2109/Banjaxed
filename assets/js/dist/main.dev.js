91a11378fb659af8f7578f8d607db4172b4d9fd2
Vitaliy2109 <murikv8@gmail.com>
2023-03-11 17:20:51 +0200

    first commit

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function fireActive() {
+  document.getElementById("forest-wrap").classList.toggle("active");
+  document.getElementById("branch-wrap").classList.toggle("animate__shakeX");
+  document.getElementById("box").classList.toggle("animate__zoomOutLeft");
+  document.getElementById("click").classList.toggle("static");
+  setTimeout(function () {
+    document.getElementById("leaf-3").classList.toggle("animate__rollOut-leaf-3");
+  }, 470);
+  setTimeout(function () {
+    document.getElementById("leaf-1").classList.toggle("animate__rollOut");
+  }, 670);
+  setTimeout(function () {
+    document.getElementById("leaf-2").classList.toggle("animate__hinge");
+  }, 800);
+}
+
+function scaleimages(id) {
+  document.getElementById(id).classList.toggle("active");
+  document.getElementById("bg").classList.toggle("active");
+}
